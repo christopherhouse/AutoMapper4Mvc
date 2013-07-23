13afc1f0b2793987e2a60ea8c7209ae8bd7f926f
Christopher House <chris.house.00@gmail.com>
2013-07-23 08:47:35 -0500

    Added references, skeleton classes

---
+++ ThirteenDaysAWeek.AutoMapper4Mvc/ActionFilters/Http/AutoMapAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.Http.Filters;
+
+namespace ThirteenDaysAWeek.AutoMapper4Mvc.ActionFilters.Http
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class AutoMapAttribute : ActionFilterAttribute
+    {
+    }
+}
---
+++ ThirteenDaysAWeek.AutoMapper4Mvc/ActionFilters/Mvc/AutoMapAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ThirteenDaysAWeek.AutoMapper4Mvc.ActionFilters.Mvc
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class AutoMapAttribute : ActionFilterAttribute
+    {
+    }
+}
