3bdddb1da799695e085af7753cb298db859bc793
Christopher House <chris.house.00@gmail.com>
2013-07-23 09:19:09 -0500

    Implemented mvc attribute ctor

---
+++ ThirteenDaysAWeek.AutoMapper4Mvc.Tests/ActionFilters/Mvc/AutoMapAttributeTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirteenDaysAWeek.AutoMapper4Mvc.ActionFilters.Mvc;
+
+namespace ThirteenDaysAWeek.AutoMapper4Mvc.Tests.ActionFilters.Mvc
+{
+    [TestClass()]
+    public class AutoMapAttributeTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_Should_Throw_ArgumemtNullException_If_SourceType_Is_Null()
+        {
+            // Arrange
+            Type sourceType = null;
+            Type destinationType = typeof (object);
+
+            // Act
+            AutoMapAttribute attribute = new AutoMapAttribute(sourceType, destinationType);
+
+            // Assert
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_Should_Throw_ArgumemtNullException_If_DestinationType_Is_Null()
+        {
+            // Arrange
+            Type sourceType = typeof (object);
+            Type destinationType = null;
+
+            // Act
+            AutoMapAttribute attribute = new AutoMapAttribute(sourceType, destinationType);
+
+            // Assert
+        }
+
+        [TestMethod()]
+        public void Constructor_Should_Set_SourceType_Property()
+        {
+            // Arrange
+            Type sourceType = typeof (int);
+            Type destinationType = typeof (string);
+
+            // Act
+            AutoMapAttribute attribute = new AutoMapAttribute(sourceType, destinationType);
+
+            // Assert
+            Assert.AreEqual(sourceType, attribute.SourceType);
+        }
+
+        [TestMethod()]
+        public void Constructor_Should_Set_DestinationType_Property()
+        {
+            // Arrange
+            Type sourceType = typeof (int);
+            Type destinationType = typeof (string);
+
+            // Act
+            AutoMapAttribute attribute = new AutoMapAttribute(sourceType, destinationType);
+
+            // Assert
+            Assert.AreEqual(destinationType, attribute.DestinationType);
+        }
+    }
+}
--- ThirteenDaysAWeek.AutoMapper4Mvc/ActionFilters/Mvc/AutoMapAttribute.cs
+++ ThirteenDaysAWeek.AutoMapper4Mvc/ActionFilters/Mvc/AutoMapAttribute.cs
@@ -6,9 +6,25 @@ namespace ThirteenDaysAWeek.AutoMapper4Mvc.ActionFilters.Mvc
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class AutoMapAttribute : ActionFilterAttribute
     {
-        public AutoMapAttribute()
+        public AutoMapAttribute(Type sourceType, Type destinationType)
         {
-            throw new NotImplementedException();
+            if (sourceType == null)
+            {
+                throw new ArgumentNullException("sourceType");
+            }
+
+            if (destinationType == null)
+            {
+                throw new ArgumentNullException("destinationType");
+            }
+
+            SourceType = sourceType;
+
+            DestinationType = destinationType;
         }
+
+        public Type SourceType { get; private set; }
+
+        public Type DestinationType { get; private set; }
     }
 }
