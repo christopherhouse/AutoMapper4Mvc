707ded4489271ff586a2d1f1bd21b2d59a3b40ea
Christopher House <chris.house.00@gmail.com>
2013-07-29 09:58:11 -0500

    Added ProfileLoader

---
+++ ThirteenDaysAWeek.AutoMapper4Mvc/Configuration/ProfileLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+namespace ThirteenDaysAWeek.AutoMapper4Mvc.Configuration
+{
+    public static class ProfileLoader
+    {
+        public static void LoadProfiles()
+        {
+            Assembly[] assembliesToScan = AppDomain.CurrentDomain.GetAssemblies();
+            Type profileType = typeof (Profile);
+
+            IEnumerable<Type> profiles = assembliesToScan.SelectMany(assembly => assembly.GetTypes())
+                               .Where(type => profileType.IsAssignableFrom(type) && !type.IsAbstract && type != profileType);
+
+            Mapper.Initialize(configuration =>
+                {
+                    foreach (Type profile in profiles)
+                    {
+                        configuration.AddProfile((Profile) Activator.CreateInstance(profile));
+                    }
+                });
+        }
+    }
+}
